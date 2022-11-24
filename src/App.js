391cf95b1fba5e290e5efb77ea68b2461d92b17a
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-24 18:30:43 -0300

    Alteração do reset.css para reset.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
-import Game from "./Game";
-import Guess from "./Guess";
-import Letters from "./Letters";
+//import Game from "./Game";
+//import Guess from "./Guess";
+//import Letters from "./Letters";
 
 import AppStyles from "./assets/styles/AppStyles";
-import "./assets/styles/reset.css";
+import Reset from "./assets/styles/Reset";
 
-const App = () => (
-    <AppStyles.GameDiv>
-        <Game></Game>
-        <Letters></Letters>
-        <Guess></Guess>
-    </AppStyles.GameDiv>
-);
+const App = () => {
+
+    return (
+        <AppStyles.GameDiv>
+            <Reset></Reset>
+            {/*
+            <Game hangmanImage={hangmanImage}></Game>
+            <Letters></Letters>
+            <Guess></Guess>
+            */}
+        </AppStyles.GameDiv>
+    );
+};
 
 export default App;
--- src/assets/styles/Reset.js
+++ src/assets/styles/Reset.js
@@ -1,3 +1,6 @@
+import { createGlobalStyle } from "styled-components";
+
+const Reset = createGlobalStyle`
 html, body, div, span, applet, object, iframe,
 h1, h2, h3, h4, h5, h6, p, blockquote, pre,
 a, abbr, acronym, address, big, cite, code,
@@ -43,3 +46,6 @@ table {
 strong {
 	font-weight: bold;
 }
+`;
+
+export default Reset;

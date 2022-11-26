9f6d0c0f3bf82ad836c61918d140ce00ef4e0df9
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-26 19:16:37 -0300

    Adicionado seletores para teste automatizado

--- src/Game.js
+++ src/Game.js
@@ -3,11 +3,11 @@ import GameStyles from "./assets/styles/GameStyles";
 const Game = (props) => (
     <GameStyles.GameItems>
         <GameStyles.LeftGameItem>
-            <GameStyles.HangmanImage src={props.hangmanImage}></GameStyles.HangmanImage>
+            <GameStyles.HangmanImage src={props.hangmanImage} data-test="game-image"></GameStyles.HangmanImage>
         </GameStyles.LeftGameItem>
         <GameStyles.RightGameItem>
-            <GameStyles.StartGameButton onClick={props.chooseWord}>Escolher Palavra</GameStyles.StartGameButton>
-            <GameStyles.WordDiv>
+            <GameStyles.StartGameButton onClick={props.chooseWord} data-test="choose-word">Escolher Palavra</GameStyles.StartGameButton>
+            <GameStyles.WordDiv data-test="word" data-answer={props.wordProgress.map(letterProgress => letterProgress.letter).join("")}>
                 {props.wordProgress.map(word => <GameStyles.SpanWord key={"letter_index_" + Array.prototype.indexOf.call(props.wordProgress, word)} success={word.success}>{word.isDiscovered  ? word.letter : "_"}</GameStyles.SpanWord>)}
             </GameStyles.WordDiv>
         </GameStyles.RightGameItem>

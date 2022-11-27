fece8d725b64daf4d91e3fe16a50790306833f93
Mathews Martins Sant'Ana Lima <thewslima167@outlook.com>
2022-11-27 13:07:49 -0300

    Correções sugeridas pelo avaliador 1

--- src/Guess.js
+++ src/Guess.js
@@ -3,8 +3,8 @@ import GuessStyles from "./assets/styles/GuessStyles";
 const Guess = (props) => (
     <GuessStyles.GuessDiv>
         <GuessStyles.KnowWordP>Já sei a palavra!</GuessStyles.KnowWordP>
-        <GuessStyles.GuessInput value={props.guessInput} onChange={(inputValue) => { props.setGuessInput(inputValue.target.value.replaceAll(" ", "")) } } data-test="guess-input"></GuessStyles.GuessInput>
-        <GuessStyles.GuessButton onClick={() => { props.guess() }} data-test="guess-button">Chutar!</GuessStyles.GuessButton>
+        <GuessStyles.GuessInput value={props.guessInput} onChange={(inputValue) => { props.setGuessInput(inputValue.target.value.replaceAll(" ", "")) } } data-test="guess-input" disabled={props.isDisabled}></GuessStyles.GuessInput>
+        <GuessStyles.GuessButton onClick={() => { props.guess() }} data-test="guess-button" disabled={props.isDisabled}>Chutar!</GuessStyles.GuessButton>
     </GuessStyles.GuessDiv>
 );
 
